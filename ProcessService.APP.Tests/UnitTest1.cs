3d30adb4290fa8922226bdd9038824f2ac27953e
bruna.goncalves <pissara2@gmail.com>
2025-04-15 18:19:06 -0300

    Add coverlet package and test project

---
+++ ProcessService.APP.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ProcessService.APP.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
